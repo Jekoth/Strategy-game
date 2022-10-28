16c531df46d5ca532e17be80da8fbc90aa338a30
Jekoth <80098768+Jekoth@users.noreply.github.com>
2022-10-28 06:45:13 +0300

    Little changes
    Added heal
    Added block
    
    Better attack menu

--- Strategy game/Program.cs
+++ Strategy game/Program.cs
@@ -1,50 +1,90 @@
-﻿internal class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-
+        
         // Declare variables
         int warriorHealth = 100;
         int skeletonHealth = 100;
+
+        int healAmount = 5;
+
+        Random random = new Random();
 
         Console.WriteLine("Battle Begins!" + " Human warrior against Skeleton warrior.");
 
         while (warriorHealth > 0 && skeletonHealth > 0)
         {
-
+            // Player turn
             // Inform player of current game state
+            
             Console.WriteLine("The human warrior has " + warriorHealth + " health.");
-            Console.WriteLine("The skeleton warrior has " + skeletonHealth + " health.");
-            Console.WriteLine("Write 'attack' to attack the enemy or 'block' to block an enemy attack");
+            Console.WriteLine("The skeleton warrior has " + skeletonHealth + " health.\n");
+            Console.WriteLine("What would like you to do?\n\n 1. Attack\n 2. Block\n 3. Heal");
 
             // Get the player's choice
-            string choice = Console.ReadLine();
+            string choice = Console.ReadLine()!;
+
             // Decide enemy damage
             int enemyDamage = new Random().Next(5, 10);
 
             // Process player action
-            if (choice == "attack")
+            Console.WriteLine("\n-- Player turn --");
+            if (choice == "1")
             {
                 //If the player attacks
-                Console.WriteLine("Human warrior attacks Skeleton warrior, dealing 20 damage.");
+                
+                Console.WriteLine("Human warrior attacks Skeleton warrior, dealing 20 damage.\n");
                 skeletonHealth -= 20;
-                Console.WriteLine("Skeleton warrior attack Human warrior dealing " + enemyDamage + " damage!");
-                warriorHealth -= enemyDamage;
             }
+                else if (choice == "2")
+                {
+                    // If the player defends
+                    Console.WriteLine("You blocked against the attack!");
+                    Console.WriteLine("The enemy would've dealt " + enemyDamage + " damage!\n");
+                    enemyDamage -= enemyDamage;
+                }
+                    else if (choice == "3")
+                    {
+                         // If the player heals
+                         warriorHealth += healAmount;
+                         Console.WriteLine("You healed " + healAmount + " yourself!\n");
+                    }
+                        else
+                        {
+                            Console.WriteLine("You choose something else.");
+                            return;
+                        }
 
-
-            if (choice == "block")
+            // Enemy turn
+            if (skeletonHealth > 0)
             {
-                // If the player defends
-                Console.WriteLine("You blocked against the attack!");
-                Console.WriteLine("The enemy would've dealt " + enemyDamage + " damage!");
-            }
+                Console.WriteLine("-- Enemy turn --");
+                int enemyChoice = random.Next(0, 2);
 
+                if (enemyChoice == 0)
+                {
+                    warriorHealth -= enemyDamage;
+                    Console.WriteLine("Skeleton warrior attack Human warrior dealing " + enemyDamage + " damage!\n");
+                }
+                    else
+                    {
+                        skeletonHealth += healAmount;
+                        Console.WriteLine("Skeleton Warrior restored " + healAmount + " health points!\n");
+                    }
+            }
         }
-        if (skeletonHealth <= 0)
+
+        // Player won
+        if (warriorHealth >= 0)
         {
             Console.WriteLine("Skeleton warrior is defeated!");
             Console.WriteLine("Human warrior is victorious!");
         }
+            // Player lost
+            else
+            {
+                Console.WriteLine("You Lose!");
+            }
     }
 }
