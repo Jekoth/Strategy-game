98596d9fa388f60a8149a6787c4ca84edbe8b3f0
Jekoth <80098768+Jekoth@users.noreply.github.com>
2023-01-31 02:08:03 +0200

    undo
    Added undo

---
+++ Strategy game/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_game
+{
+    class PlayerMove
+    {
+        public string Type { get; set; }
+        public int Damage { get; set; }
+        public int Health { get; set; }
+
+        public PlayerMove(string type, int damage, int health)
+        {
+            this.Type = type;
+            this.Damage = damage;
+            this.Health = health;
+        }
+    }
+
+
+}
--- Strategy game/Program.cs
+++ Strategy game/Program.cs
@@ -1,4 +1,6 @@
-﻿using Strategy_game;
+﻿using System;
+using System.Collections.Generic;
+using Strategy_game;
 
 class Program
 {
@@ -14,71 +16,88 @@ class Program
 
         Random random = new Random();
 
+        // initialize a new stack of PlayerMove objects
+        Stack<PlayerMove> moves = new Stack<PlayerMove>();
+
         Console.WriteLine("Battle begins! Human warrior against Skeleton warrior.");
 
         while (player.playerHealth > 0 && enemy.enemyHealth > 0)
         {
             // Player turn
             // Inform player of current game state
-            
+
             Console.WriteLine("The human warrior has " + player.playerHealth + " health.");
             Console.WriteLine("The skeleton warrior has " + enemy.enemyHealth + " health.\n");
-            Console.WriteLine("What would like you to do?\n\n 1. Attack\n 2. Heal");
+            Console.WriteLine("What would like you to do?\n\n 1. Attack\n 2. Heal\n 3. Undo");
 
             // Get the player's choice
             string choice = Console.ReadLine()!;
+
 
             // Process player action
             Console.WriteLine("\n-- Player turn --");
             if (choice == "1")
             {
                 //If the player attacks
-                
+                moves.Push(new PlayerMove("attack", player.playerDamage, enemy.enemyHealth));
                 Console.WriteLine("Human warrior attacks Skeleton warrior, dealing " + player.playerDamage + " damage.\n");
                 enemy.enemyHealth -= player.playerDamage;
             }
-                //else if (choice == "2")
-                //{
-                //    // If the player defends
-                //    Console.WriteLine("You blocked against the attack!");
-                //    Console.WriteLine("The enemy would've dealt " + enemy.enemyDamage + " damage!\n");
-                //    enemy.enemyDamage -= enemy.enemyDamage;
-                //}
-                    else if (choice == "2")
-                    {
-                         // If the player heals
-                         player.playerHealth += healAmount;
-                         Console.WriteLine("You healed " + healAmount + " yourself!\n");
-                    }
-                        else
-                        {
-                            Console.WriteLine("You choose something else.");
-                            return;
-                        }
-
-            // Enemy turn
-            if (enemy.enemyHealth > 0)
+            else if (choice == "2")
             {
-                Console.WriteLine("-- Enemy turn --");
-                int enemyChoice = random.Next(0, 2);
-
-                if (enemyChoice == 0)
+                // If the player heals
+                moves.Push(new PlayerMove("heal", healAmount, player.playerHealth));
+                player.playerHealth += healAmount;
+                Console.WriteLine("You healed " + healAmount + " yourself!\n");
+            }
+            else if (choice == "3")
+            {
+                if (moves.Count > 0)
                 {
-                    player.playerHealth -= enemy.enemyDamage;
-                    Console.WriteLine("Skeleton warrior attack Human warrior dealing " + enemy.enemyDamage + " damage!\n");
+                    PlayerMove lastMove = moves.Pop();
+                    if (lastMove.Type == "attack")
+                    {
+                        enemy.enemyHealth = lastMove.Health;
+                        Console.WriteLine("Undo successful: Attack undone. Skeleton warrior health: " + enemy.enemyHealth);
+                    }
+                    else if (lastMove.Type == "heal")
+                    {
+                        player.playerHealth = lastMove.Health;
+                        Console.WriteLine("Undo successful: Heal undone. Player health: " + player.playerHealth);
+                    }
                 }
-                  else /*if(enemyChoice >= 1)*/
-                  {
-                     enemy.enemyHealth += healAmount;
-                     Console.WriteLine("Skeleton Warrior restored " + healAmount + " health points!\n");
-                  }
-                    //else
-                    //{
-                    //    // If the enemy defends
-                    //    Console.WriteLine("Enemy blocked against the attack!");
-                    //    Console.WriteLine("You would've dealt " + player.playerDamage + " damage!\n");
-                    //    player.playerDamage -= player.playerDamage;
-                    //}
+                else
+                {
+                    Console.WriteLine("Nothing to undo.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+                return;
+            }
+
+            // Check if player chose option 3 (undo move)
+            if (choice != "3")
+            {
+                // Enemy turn
+                if (enemy.enemyHealth > 0)
+                {
+                    Console.WriteLine("-- Enemy turn --");
+                    int enemyChoice = random.Next(0, 2);
+
+                    if (enemyChoice == 0)
+                    {
+                        moves.Push(new PlayerMove("attack", player.playerDamage, enemy.enemyHealth));
+                        Console.WriteLine("Skeleton warrior attack Human warrior dealing " + enemy.enemyDamage + " damage!\n");
+                        player.playerHealth -= enemy.enemyDamage;
+                    }
+                    else if (enemyChoice >= 1)
+                    {
+                        enemy.enemyHealth += healAmount;
+                        Console.WriteLine("Skeleton Warrior restored " + healAmount + " health points!\n");
+                    }
+                }
             }
         }
 
@@ -88,10 +107,10 @@ class Program
             Console.WriteLine("Skeleton warrior is defeated!");
             Console.WriteLine("Human warrior is victorious!");
         }
-            // Player lost
-            else
-            {
-                Console.WriteLine("You Lose!");
-            }
+        // Player lost
+        else
+        {
+            Console.WriteLine("You Lose!");
+        }
     }
 }
